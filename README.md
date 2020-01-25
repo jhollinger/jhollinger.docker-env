bcf001d884a2cffc76bfb5363071ddcb18d795d5
Jordan Hollinger <jordan.hollinger@wellsky.com>
2020-01-25 12:29:29 -0500

    Initial commit - commands to set up env

---
+++ README.md
@@ -0,0 +1,24 @@
+# Docker Env
+
+**Requirements**
+
+* git
+* docker
+* docker-compose
+
+## Dev Environment
+
+### Setup
+
+    git clone git@github.com:jhollinger/jhollinger.docker-env.git
+    cd jhollinger.docker-env
+    bin/setup-dev
+    source bin/helpers.sh
+
+### Use
+
+TODO
+
+## Prod Environment
+
+TODO
---
+++ bin/helpers.sh
@@ -0,0 +1,24 @@
+root_dir="${PWD}/$(dirname $0)/.."
+
+#
+# Run any docker-compose command using the specified environment.
+#
+# Example: jh-compose dev up
+#
+function jh-compose {
+  env_name=${1}
+  shift
+  echo "docker-compose -f ${root_dir}/docker-compose.yml -f ${root_dir}/docker-compose.${env_name}.yml $@"
+  docker-compose -f "${root_dir}/docker-compose.yml" -f "${root_dir}/docker-compose.${env_name}.yml" "$@"
+}
+
+#
+# Run a command inside a docker container.
+#
+# Example: jh-run dev cur8 bundle exec rake db:setup
+#
+function jh-run {
+  env_name=${1}
+  shift
+  jh-compose ${env_name} run --rm "$@"
+}
